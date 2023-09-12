740eae6e268d01a2ff72a4b016858bab58739f5a
Nathan Dancoine <ndancoin@c2r3p7.42perpignan.fr>
2023-09-12 12:50:39 +0200

    modif c05ex03

--- c/c05/ex03/ft_recursive_power.c
+++ c/c05/ex03/ft_recursive_power.c
@@ -6,16 +6,18 @@
 /*   By: ndancoin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 16:06:32 by ndancoin          #+#    #+#             */
-/*   Updated: 2023/09/07 17:40:15 by ndancoin         ###   ########.fr       */
+/*   Updated: 2023/09/12 12:33:36 by ndancoin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_power(int nb, int power)
 {
-	if (power != 0)
-		return (nb * ft_recursive_power(nb, power - 1));
-	else
+	if (power < 0)
+		return (0);
+	else if (power < 1)
 		return (1);
+	else
+		return (nb * ft_recursive_power(nb, power - 1));
 }
 /*
 #include <stdio.h>
