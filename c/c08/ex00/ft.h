badbdec7c377ee8b96f567233d4b4a345a6de937
Nathan Dancoine <ndancoin@c2r3p7.42perpignan.fr>
2023-09-12 18:33:25 +0200

    c08 0912_1833

---
+++ c/c08/ex00/ft.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndancoin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 13:01:04 by ndancoin          #+#    #+#             */
+/*   Updated: 2023/09/12 15:04:39 by ndancoin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+#endif
---
+++ c/c08/ex01/ft_boolean.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndancoin <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 13:08:23 by ndancoin          #+#    #+#             */
+/*   Updated: 2023/09/12 15:41:02 by ndancoin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+# define SUCCESS 0
+# define FALSE 0
+# define TRUE 1
+# define EVEN(nbr) ((nbr) % 2 == 0)
+
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+
+typedef int	t_bool;
+
+#endif
